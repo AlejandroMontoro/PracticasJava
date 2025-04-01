bf80ef7e51b4ce3e060f1f0b3a930a60dcc95fbc
Isidro Alejandro Montoro Perez <ale_mpi@hotmail.com>
2025-04-01 16:47:39 -0600

    Prueba 2

--- Sistema.java
+++ Sistema.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Sistema {
     public static void main(String args[]) {
-
+ 
         //Sistema de vacaciones
         Scanner entrada = new Scanner(System.in);
         String nombre = "", departamento = "";
